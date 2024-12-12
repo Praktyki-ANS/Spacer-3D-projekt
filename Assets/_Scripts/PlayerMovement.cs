4d0e61086c9d80159ba576ac037a82c39a292e62
Jakub Lebioda <144003766+LebiodaJakub@users.noreply.github.com>
2024-12-12 12:59:52 +0100

    Fixed Inside Scene
    Naprawiono elementy sceny
    Naprawiono Prefab gracza

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,55 +1,76 @@
 using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float movementSpeed = 5;
-    [SerializeField] float mouseSensitivity = 10;
-    Transform camTransform;
-    float mouseX, mouseY, xRotation;
+    [Header("PlayerScript Movement")]
+    [SerializeField] float playerSpeed = 5f;
+    
+    [Header("PlayerScript Jump")]
+    [SerializeField] float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = Physics.gravity.y;
+    [SerializeField] Vector3 playerVelocity;
+    private bool groundedPlayer;
+    
+    [Header("PlayerScript Look")]
+    [SerializeField] Camera playerCamera;
+    [SerializeField] float mouseSensitivity = 1f;
+    private float xRotation = 0f;
 
-    // Start is called before the first frame update
-    void Start()
+
+    private CharacterController controller;
+
+    private void Start()
     {
-        camTransform = transform.GetChild(0);
+        controller = GetComponent<CharacterController>();
+        playerCamera = GetComponentInChildren<Camera>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        MovePlayer();
-        LookPlayer();
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y <= 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        PlayerMove();
+        PlayerLook();
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+            PlayerJump();
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 
-    void MovePlayer()
+    void PlayerMove()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += transform.forward * movementSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position -= transform.forward * movementSpeed * Time.deltaTime;
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            transform.position += transform.right * movementSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position -= transform.right * movementSpeed * Time.deltaTime;
-        }
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(playerSpeed * Time.deltaTime * move);
     }
 
-    void LookPlayer()
+    void PlayerLook()
     {
-        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        // Get mouse input
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90, 90);
-
-        camTransform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        // Rotate the player's body left and right
         transform.Rotate(Vector3.up * mouseX);
 
+        // Rotate the camera up and down (clamping to 90 degrees)
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+    void PlayerJump()
+    {
+        playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
     }
 }
