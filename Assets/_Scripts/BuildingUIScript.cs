62e2e3c85fbbd22e89882429990114c4c7a2bf0e
Jakub Lebioda <144003766+LebiodaJakub@users.noreply.github.com>
2024-12-19 14:12:54 +0100

    Updating selected building's info
    Zmiana budynku teraz aktualizuje informacje w panelu głównym

--- Assets/_Scripts/BuildingUIScript.cs
+++ Assets/_Scripts/BuildingUIScript.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -8,6 +8,7 @@ public class BuildingUIScript : MonoBehaviour
     public BuildingData BuildingData;
     [SerializeField] TMP_Text NameTextField;
     [SerializeField] TMP_Text AddressTextField;
+    public static event Action<BuildingUIScript> OnBuildingChanged;
 
     public string Name {  get; private set; }
     public string Address { get; private set; }
@@ -17,7 +18,7 @@ public class BuildingUIScript : MonoBehaviour
     {
         Name = BuildingData.Name;
         BuildingID = BuildingData.BuildingID;
-        Address = CompileAddress();
+        Address = CompileAddress(BuildingData);
         SetUIInfo();
     }
 
@@ -27,15 +28,20 @@ public class BuildingUIScript : MonoBehaviour
         AddressTextField.text = Address;
     }
 
-    string CompileAddress()
+    public string CompileAddress(BuildingData buildingData)
     {
-        string appartmentnumber = BuildingData.ApartmentNmber;
+        string appartmentnumber = buildingData.ApartmentNmber;
         string address = "";
 
-        address += $"{BuildingData.StreetName} {BuildingData.BuildingNumber}";
+        address += $"{buildingData.StreetName} {buildingData.BuildingNumber}";
         if (appartmentnumber != null && appartmentnumber != "")
             address += $"/{appartmentnumber} ";
-        address += $",\n{BuildingData.PostalCode} {BuildingData.CityName}";
+        address += $",\n{buildingData.PostalCode} {buildingData.CityName}";
         return address;
+    }
+
+    public void SetCurrentBuilding()
+    {
+        OnBuildingChanged?.Invoke(this);
     }
 }
